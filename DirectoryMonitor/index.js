581ecea2e160f8f8b234d5008927f53a418397d8
Aishwarya R <RAishwarya1103@gmail.com>
2020-09-15 16:51:36 +0530

    handled logic when no active config details are fetched

--- DirectoryMonitor/index.js
+++ DirectoryMonitor/index.js
@@ -11,49 +11,54 @@ async function getConfigAndStartTask() {
   logger.debug(`Entered function getConfigAndStartTask`);
   try {
     const response = await serviceController.getData(getActiveConfigURL);
-    const magicString = response.config[0].magicString;
-    const directory = response.config[0].directory;
-    const configId = response.config[0].id;
-    const interval = +response.config[0].interval;
-    const period = response.config[0].period;
-    let intervalSchedule = "";
+    if (response.config.length > 0) {
+      const magicString = response.config[0].magicString;
+      const directory = response.config[0].directory;
+      const configId = response.config[0].id;
+      const interval = +response.config[0].interval;
+      const period = response.config[0].period;
+      let intervalSchedule = "";
 
-    switch (period) {
-      case "seconds":
-        intervalSchedule = `*/${interval} * * * * *`;
-        break;
-      case "minutes":
-        intervalSchedule = `*/${interval} * * * *`;
-        break;
-      case "hours":
-        intervalSchedule = `* */${interval} * * *`;
-        break;
-      case "date":
-        intervalSchedule = `* * ${interval} * *`;
-        break;
-      case "month":
-        intervalSchedule = `* * * ${interval} *`;
-        break;
-      case "week":
-        intervalSchedule = `* * * * ${interval}`;
-        break;
-    }
-    logger.info(`Scheduler details: ${period}: ${intervalSchedule}`);
-    if (cron.validate(intervalSchedule)) {
-      let task = cron.schedule(
-        intervalSchedule,
-        () => {
-          const startTime = new Date().toISOString();
-          DirectoryMonitor(magicString, directory, configId, startTime);
-        },
-        {
-          scheduled: false,
-        }
-      );
+      switch (period) {
+        case "seconds":
+          intervalSchedule = `*/${interval} * * * * *`;
+          break;
+        case "minutes":
+          intervalSchedule = `*/${interval} * * * *`;
+          break;
+        case "hours":
+          intervalSchedule = `* */${interval} * * *`;
+          break;
+        case "date":
+          intervalSchedule = `* * ${interval} * *`;
+          break;
+        case "month":
+          intervalSchedule = `* * * ${interval} *`;
+          break;
+        case "week":
+          intervalSchedule = `* * * * ${interval}`;
+          break;
+      }
+      logger.info(`Scheduler details: ${period}: ${intervalSchedule}`);
+      if (cron.validate(intervalSchedule)) {
+        let task = cron.schedule(
+          intervalSchedule,
+          () => {
+            const startTime = new Date().toISOString();
+            DirectoryMonitor(magicString, directory, configId, startTime);
+          },
+          {
+            scheduled: false,
+          }
+        );
 
-      task.start();
+        task.start();
+      } else {
+        logger.error(`Invalid interval cron scheduler`);
+        return;
+      }
     } else {
-      logger.error(`Invalid interval cron scheduler`);
+      logger.error(`No active configutaion details available.`);
       return;
     }
   } catch (err) {
